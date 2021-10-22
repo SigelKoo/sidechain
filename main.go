963e36ca26d98b7592bb127b7098b312e800db99
SigelKoo <352326510@qq.com>
2021-10-22 16:08:55 +0800

    change file

--- main.go
+++ main.go
@@ -1,42 +1,10 @@
 package main
 
 import (
-	"fmt"
-	"os"
-	sdkInit "sidechain/sdkinit"
 	sidechain "sidechain/src"
 )
 
-var App sdkInit.Application
-
-
 func main() {
-	orgs := []*sdkInit.OrgInfo{
-		{
-			OrgAdminUser:  "Admin",
-			OrgName:       "Org1",
-			OrgMspId:      "Org1MSP",
-			OrgUser:       "User1",
-			OrgPeerNum:    2,
-		},
-	}
-
-	// init sdk env info
-	info := sdkInit.SdkEnvInfo{
-		ChannelID:        "mychannel",
-		Orgs:             orgs,
-		OrdererAdminUser: "Admin",
-		OrdererOrgName:   "OrdererOrg",
-		OrdererEndpoint:  "orderer.example.com",
-	}
-
-	// sdk setup
-	sdk, err := sdkInit.Setup("./config/crypto-config.yaml", &info)
-	if err != nil {
-		fmt.Println(">> SDK setup error:", err)
-		os.Exit(-1)
-	}
-	fmt.Println(sdk)
 	sidechain.InitChainBrowserService()
 	sidechain.QueryLatestBlocksInfo()
 }
