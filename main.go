c1791a8dfbbce761827d1b558bf5a6cd036f9bac
SigelKoo <352326510@qq.com>
2021-10-22 11:30:28 +0800

    add md

--- main.go
+++ main.go
@@ -1,8 +1,43 @@
 package main
 
-import sidechain "sidechain/src"
+import (
+	"fmt"
+	"os"
+	sdkInit "sidechain/sdkinit"
+	sidechain "sidechain/src"
+)
+
+var App sdkInit.Application
+
 
 func main() {
+	orgs := []*sdkInit.OrgInfo{
+		{
+			OrgAdminUser:  "Admin",
+			OrgName:       "Org1",
+			OrgMspId:      "Org1MSP",
+			OrgUser:       "User1",
+			OrgPeerNum:    2,
+		},
+	}
+
+	// init sdk env info
+	info := sdkInit.SdkEnvInfo{
+		ChannelID:        "mychannel",
+		ChannelConfig:    "./fixtures/channel-artifacts/channel.tx",
+		Orgs:             orgs,
+		OrdererAdminUser: "Admin",
+		OrdererOrgName:   "OrdererOrg",
+		OrdererEndpoint:  "orderer.example.com",
+	}
+
+	// sdk setup
+	sdk, err := sdkInit.Setup("config.yaml", &info)
+	if err != nil {
+		fmt.Println(">> SDK setup error:", err)
+		os.Exit(-1)
+	}
+	fmt.Println(sdk)
 	sidechain.InitChainBrowserService()
 	sidechain.QueryLatestBlocksInfo()
 }
