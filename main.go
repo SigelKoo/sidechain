6d01e18812ddfe6ac6164cde82d8f6de00f7754b
SigelKoo <352326510@qq.com>
2021-10-26 16:48:20 +0800

    add md

--- main.go
+++ main.go
@@ -1,7 +1,10 @@
 package main
 
-import "sidechain/ethContract"
+import (
+	"fmt"
+	"sidechain/ethContract"
+)
 
 func main() {
-	ethContract.ContractDeploy("HTTP://192.168.132.80:8501", "2e8749fd1ba7a42586d2bb38c10fab2e8845abd7733378a95a03fdcdbd1b854e")
+	fmt.Println(ethContract.ContractDeploy("HTTP://192.168.132.80:8501", "2e8749fd1ba7a42586d2bb38c10fab2e8845abd7733378a95a03fdcdbd1b854e"))
 }
