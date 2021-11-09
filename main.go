4f933280a04cfdcd5d4dcf62091e5911edcf77f9
SigelKoo <352326510@qq.com>
2021-11-09 21:32:59 +0800

    change main

--- main.go
+++ main.go
@@ -1,11 +1,11 @@
 package main
 
 import (
-	// ethsdk "sidechain/ethSDK"
-	sc "sidechain/src"
+	"fmt"
+	ethsdk "sidechain/ethSDK"
 )
 
 func main() {
-	// fmt.Println(ethsdk.ContractDeploy("HTTP://192.168.132.80:8501", "2e8749fd1ba7a42586d2bb38c10fab2e8845abd7733378a95a03fdcdbd1b854e"))
-	sc.RegisterUser("123", "Org1", "123")
+	fmt.Println(ethsdk.ContractDeploy("HTTP://192.168.132.80:8501", "2e8749fd1ba7a42586d2bb38c10fab2e8845abd7733378a95a03fdcdbd1b854e"))
+	// sc.RegisterUser("123", "Org1", "123")
 }
