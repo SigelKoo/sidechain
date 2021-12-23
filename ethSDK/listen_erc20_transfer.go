79e849e164ff2be8aa18963976fb3a8114268478
SigelKoo <352326510@qq.com>
2021-12-23 11:14:51 +0800

    change ccEvent payload

--- ethSDK/listen_erc20_transfer.go
+++ ethSDK/listen_erc20_transfer.go
@@ -158,6 +158,7 @@ func Eth_listen_erc20_transfer(url string, address string) {
 					if err != nil {
 						fmt.Println(err.Error())
 					} else {
+
 						fmt.Println("转账成功，交易编号为：" + msg)
 					}
 				}
--- fabricSDK/domain.go
+++ fabricSDK/domain.go
@@ -1,6 +1,7 @@
 package fabricSDK
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/hyperledger/fabric-sdk-go/pkg/client/channel"
 	"github.com/hyperledger/fabric-sdk-go/pkg/common/providers/fab"
@@ -10,6 +11,12 @@ import (
 type ServiceSetup struct {
 	ChaincodeID string
 	Client      *channel.Client
+}
+
+type event struct {
+	From  string `json:"from"`
+	To    string `json:"to"`
+	Value int    `json:"value"`
 }
 
 func regitserEvent(client *channel.Client, chaincodeID, eventID string) (fab.Registration, <-chan *fab.CCEvent) {
@@ -25,6 +32,11 @@ func eventResult(notifier <-chan *fab.CCEvent, eventID string) error {
 	select {
 	case ccEvent := <-notifier:
 		fmt.Printf("接收到链码事件: %v\n", ccEvent)
+		thisEvent := &event{}
+		json.Unmarshal(ccEvent.Payload, thisEvent)
+		fmt.Printf("from: %v\n", thisEvent.From)
+		fmt.Printf("to: %v\n", thisEvent.To)
+		fmt.Printf("value: %v\n", thisEvent.Value)
 	case <-time.After(time.Second * 20):
 		return fmt.Errorf("不能根据指定的事件ID接收到相应的链码事件(%s)", eventID)
 	}
