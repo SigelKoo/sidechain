c0310f8b196ac5954e40aca3ac7d4c6b67d72762
sigelkoo <352326510@qq.com>
2022-03-25 11:06:39 +0800

    add burn page

--- main.go
+++ main.go
@@ -31,9 +31,8 @@ func startService(done func()) {
 	r.LoadHTMLGlob("./web/**/*")
 	r.StaticFS("/css", http.Dir("./web/css"))
 	r.StaticFS("/js", http.Dir("./web/js"))
-	r.GET("/login", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "login.html", nil)
-	})
+	res1 := "无"
+	res2 := "无"
 	r.GET("/transfer", func(c *gin.Context) {
 		Org2UserInfo := fabricSDK.InitInfo{
 			ChannelID:     "mychannel",
@@ -78,17 +77,57 @@ func startService(done func()) {
 		c.HTML(http.StatusOK, "transfer.html", gin.H{
 			"token1": ethSDK.GetUserBalance("/home/eth-poa/signer1/data/geth.ipc", "0xD78d66C33933a05c57c503d61667918f95cee351", "0x416b1e5329Bd97BB704866bD489747b26848fA42"),
 			"token2": balance,
+			"res":    res1,
 		})
 	})
-	r.POST("/login", func(c *gin.Context) {
-		username := c.DefaultPostForm("username", "somebody")
-		password := c.DefaultPostForm("password", "***")
-		c.HTML(http.StatusOK, "index.html", gin.H{
-			"Name":     username,
-			"Password": password,
+	r.GET("/burn", func(c *gin.Context) {
+		Org2UserInfo := fabricSDK.InitInfo{
+			ChannelID:     "mychannel",
+			ChannelConfig: "/home/fabric-samples/test-network/channel-artifacts/mychannel.block",
+
+			OrgAdmin:       "Admin",
+			OrgName:        "Org2",
+			OrdererOrgName: "orderer.example.com",
+
+			ChaincodeID:     "token_erc20",
+			ChaincodeGoPath: os.Getenv("GOPATH"),
+			ChaincodePath:   "/home/fabric-samples/token-erc-20/chaincode-go",
+			UserName:        "0x416b1e5329Bd97BB704866bD489747b26848fA42",
+		}
+		sdk, err := fabricSDK.SetupSDK("./config/crypto-config.yaml", false)
+		if err != nil {
+			fmt.Println(err.Error())
+			return
+		}
+		defer sdk.Close()
+		clientChannelContext := sdk.ChannelContext(Org2UserInfo.ChannelID, fabsdk.WithUser(Org2UserInfo.UserName), fabsdk.WithOrg(Org2UserInfo.OrgName))
+		channelClient, err := channel.New(clientChannelContext)
+		if err != nil {
+			fmt.Println(err.Error())
+			return
+		}
+		eventClient, err := event.New(clientChannelContext, event.WithBlockEvents(), event.WithSeekType(seek.Newest))
+		if err != nil {
+			fmt.Println(err.Error())
+			return
+		}
+		client1ServiceSetup := fabricSDK.ServiceSetup{
+			ChaincodeID:   Org2UserInfo.ChaincodeID,
+			ChannelClient: channelClient,
+			EventClient:   eventClient,
+		}
+		balance, err := client1ServiceSetup.ClientAccountBalance()
+		if err != nil {
+			fmt.Println(err.Error())
+			return
+		}
+		c.HTML(http.StatusOK, "burn.html", gin.H{
+			"token1": ethSDK.GetUserBalance("/home/eth-poa/signer1/data/geth.ipc", "0xD78d66C33933a05c57c503d61667918f95cee351", "0x416b1e5329Bd97BB704866bD489747b26848fA42"),
+			"token2": balance,
+			"res":    res2,
 		})
 	})
-	r.POST("/submit", func(c *gin.Context) {
+	r.POST("/transfer", func(c *gin.Context) {
 		to := c.PostForm("to")
 		value := c.PostForm("value")
 		Org2User1Info := fabricSDK.InitInfo{
@@ -131,19 +170,88 @@ func startService(done func()) {
 
 		tranMsg, err := client1ServiceSetup.Transfer(to, value)
 		if err != nil {
+			res1 = err.Error()
 			fmt.Println(err.Error())
+			return
 		} else {
+			res1 = "转账成功，交易编号为：" + tranMsg
 			fmt.Println("转账成功，交易编号为：" + tranMsg)
 		}
 
 		balance, err := client1ServiceSetup.ClientAccountBalance()
 		if err != nil {
+			res1 = err.Error()
 			fmt.Println(err.Error())
 			return
 		}
+		// 跳转刷新页面
 		c.HTML(http.StatusOK, "transfer.html", gin.H{
 			"token1": ethSDK.GetUserBalance("/home/eth-poa/signer1/data/geth.ipc", "0xD78d66C33933a05c57c503d61667918f95cee351", "0x416b1e5329Bd97BB704866bD489747b26848fA42"),
 			"token2": balance,
+			"res":    res1,
+		})
+	})
+	r.POST("/burn", func(c *gin.Context) {
+		value := c.PostForm("value")
+		Org2User1Info := fabricSDK.InitInfo{
+			ChannelID:     "mychannel",
+			ChannelConfig: "/home/fabric-samples/test-network/channel-artifacts/mychannel.block",
+
+			OrgAdmin:       "Admin",
+			OrgName:        "Org2",
+			OrdererOrgName: "orderer.example.com",
+
+			ChaincodeID:     "token_erc20",
+			ChaincodeGoPath: os.Getenv("GOPATH"),
+			ChaincodePath:   "/home/fabric-samples/token-erc-20/chaincode-go",
+			UserName:        "0x416b1e5329Bd97BB704866bD489747b26848fA42",
+		}
+		sdk, err := fabricSDK.SetupSDK("./config/crypto-config.yaml", false)
+		if err != nil {
+			fmt.Println(err.Error())
+			return
+		}
+		defer sdk.Close()
+		client1ChannelContext := sdk.ChannelContext(Org2User1Info.ChannelID, fabsdk.WithUser(Org2User1Info.UserName), fabsdk.WithOrg(Org2User1Info.OrgName))
+		channelClient1, err := channel.New(client1ChannelContext)
+		if err != nil {
+			fmt.Println(err.Error())
+			return
+		}
+
+		eventClient1, err := event.New(client1ChannelContext, event.WithBlockEvents(), event.WithSeekType(seek.Newest))
+		if err != nil {
+			fmt.Println(err.Error())
+			return
+		}
+
+		client1ServiceSetup := fabricSDK.ServiceSetup{
+			ChaincodeID:   Org2User1Info.ChaincodeID,
+			ChannelClient: channelClient1,
+			EventClient:   eventClient1,
+		}
+
+		tranMsg, err := client1ServiceSetup.Burn(value)
+		if err != nil {
+			res2 = err.Error()
+			fmt.Println(err.Error())
+			return
+		} else {
+			res2 = "转账成功，交易编号为：" + tranMsg
+			fmt.Println("转账成功，交易编号为：" + tranMsg)
+		}
+
+		balance, err := client1ServiceSetup.ClientAccountBalance()
+		if err != nil {
+			res2 = err.Error()
+			fmt.Println(err.Error())
+			return
+		}
+		// 跳转刷新页面
+		c.HTML(http.StatusOK, "burn.html", gin.H{
+			"token1": ethSDK.GetUserBalance("/home/eth-poa/signer1/data/geth.ipc", "0xD78d66C33933a05c57c503d61667918f95cee351", "0x416b1e5329Bd97BB704866bD489747b26848fA42"),
+			"token2": balance,
+			"res":    res2,
 		})
 	})
 	r.Run(":9090")
